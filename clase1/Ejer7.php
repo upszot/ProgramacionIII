b65b682f9b0c02937bb84b17779fbc209e934479
gferrer <gferrer@mecon.gov.ar>
2019-05-07 15:38:37 -0300

    ej7

--- clase1/Ejer7.php
+++ clase1/Ejer7.php
@@ -6,13 +6,32 @@ echo "</br></br><strong>$Ap7</strong></br>";
 $Ap7='Aplicación No 7 (Mostrar fecha y estación)
 Obtenga la fecha actual del servidor (función date​ ) y luego imprímala dentro de la página con
 distintos formatos (seleccione los formatos que más le guste). Además indicar que estación del
-año es. Utilizar una estructura selectiva múltiple.
-Aplicación No 8 (Números en letras)
-Realizar un programa que en base al valor numérico de la variable ​ $num​ , pueda mostrarse por
-pantalla, el nombre del número que tenga dentro escrito con palabras, para los números entre
-el 20 y el 60.
-Por ejemplo, si $num = 43 debe mostrarse por pantalla “cuarenta y tres”.';
+año es. Utilizar una estructura selectiva múltiple.';
 
-$fecha=date();
+/*$fecha=date();
 echo "fecha= $fecha";
+*/
+echo date("Y")."<br>";
+echo date("M-D-Y")."<br>";
+echo date("d/m/y")."<br>";
+echo date("d/m")."<br>"."<br>"."<br>";
+
+if(date("m") >=1 && date("m") <=3 ) 
+{
+	print("verano");
+}
+if(date("m") >=4 && date("m") <=6 ) 
+{
+	
+	print("otoño");
+}
+if(date("m") >=7 && date("m") <=8 ) 
+{
+    print("invierno");
+}
+if(date("m") >=9 && date("m") <=12 ) 
+{
+	print("primavera");
+}
+
 ?>
