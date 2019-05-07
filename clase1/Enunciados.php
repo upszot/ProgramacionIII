c5a9c47deb6f9f3cf71e90094549f76c156836f1
gferrer <gferrer@mecon.gov.ar>
2019-05-07 15:40:21 -0300

    ej7

--- clase1/Enunciados.php
+++ clase1/Enunciados.php
@@ -36,8 +36,9 @@ resultado por pantalla.';
 $Ap7='Aplicación No 7 (Mostrar fecha y estación)
 Obtenga la fecha actual del servidor (función date​ ) y luego imprímala dentro de la página con
 distintos formatos (seleccione los formatos que más le guste). Además indicar que estación del
-año es. Utilizar una estructura selectiva múltiple.
-Aplicación No 8 (Números en letras)
+año es. Utilizar una estructura selectiva múltiple.';
+
+$Ap8='Aplicación No 8 (Números en letras)
 Realizar un programa que en base al valor numérico de la variable ​ $num​ , pueda mostrarse por
 pantalla, el nombre del número que tenga dentro escrito con palabras, para los números entre
 el 20 y el 60.
